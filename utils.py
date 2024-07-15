83a5551bf80e30e929d42085ec042ad71a62f4e6
Anton Brink <77839398+AntonBrinkCodes@users.noreply.github.com>
2024-07-15 13:19:30 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -52,7 +52,7 @@ def getDataDirectory(isDocker=False):
 
 def getOpenPoseDirectory(isDocker=False):
     computername = os.environ.get('COMPUTERNAME', None)
-    
+    print('COMPUTER NAME IS {}', computername)
     # Paths to OpenPose folder for local testing.
     if computername == "DESKTOP-0UPR1OH":
         openPoseDirectory = "C:/Software/openpose-1.7.0-binaries-win64-gpu-python3.7-flir-3d_recommended/openpose"
