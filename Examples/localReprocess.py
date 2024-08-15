e774884a4a2c9a79b29a56faf65cecb689d13dec
Anton Brink <77839398+AntonBrinkCodes@users.noreply.github.com>
2024-08-15 09:56:04 +0200

    Update localReprocess.py

--- Examples/localReprocess.py
+++ Examples/localReprocess.py
@@ -14,11 +14,10 @@ from main import main
 session_name = "4cf4bca5-7cd0-4db8-af11-5d39d485dba8" # aka session_ids in reprocessSessions.py
 
 calib_id = "36598d50-4fd8-406d-8808-4e9df3cd0e84" # None (auto-selected trial), [] (skip), or string of specific trial_id
-static_id = "neutral" # None (auto-selected trial), [] (skip), or string of specific trial_id
+static_id = "ac82774d-b679-4a90-bce1-b3b768532503" # None (auto-selected trial), [] (skip), or string of specific trial_id
 dynamic_trialNames = "afca93fd-9753-4bea-9130-5fdcf151d9f0" # None (all dynamic trials), [] (skip), or list of trial names.
 # OBS!!! Above are CaSE SenSIiVE
-
-trial_Names = "s05-treadmill_1_recording" # "calibration for extrinsic trials." neutral for static trials.
+trial_Names = "neutral" # "calibration for extrinsic trials." neutral for static trials.
 # Trial name for dynamic trials
 
 poseDetector = 'openpose'
@@ -29,7 +28,7 @@ extrinsicTrial = False
 genericFolderNames = False
 scaleModel = False
 
-sessionType = "dynamic" # Calibration, static, or dynamic. assumes dynamic if anything else
+sessionType = "static" # Calibration, static, or dynamic. assumes dynamic if anything else
 
 
 if sessionType == "calibration":
