e4ccca3cc6445282704ccce7226038aadd12e21e
Anton Brink <77839398+AntonBrinkCodes@users.noreply.github.com>
2025-05-13 10:32:00 +0200

    Update localServer.py

--- Examples/Local/localServer.py
+++ Examples/Local/localServer.py
@@ -832,7 +832,7 @@ async def handle_web_message(websocket, message_json, command, active_session: S
             sessionManager.saveSubject(subject=subject)
         elif command == "set_framerate":
             framerate = message_json.get("framerate")
-            response = {"command": "set_framerate", "trialType": "", "max_frame_rate": framerate}
+            response = {"command": "set_framerate", "trialType": "", "max_frame_rate": framerate, "session": session_id}
             print(f"Sending to set framerate as {framerate}")
             await manager.broadcast(json.dumps(response), websocket) # Send to mobiles to change frame rate :)
         elif command == "ping":
