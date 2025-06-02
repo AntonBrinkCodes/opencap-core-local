81e3a9fe87b21e5e94b7e15e27e1cff80bd40c41
Anton Brink <77839398+AntonBrinkCodes@users.noreply.github.com>
2025-06-02 11:27:55 +0200

    Update localReprocess.py

--- Examples/Local/localReprocess.py
+++ Examples/Local/localReprocess.py
@@ -69,16 +69,16 @@ def runLocalTrial(sessionId: str, trialNames, trialId, trialType="dynamic", pose
 
 
 if __name__=="__main__":
-    session_name = "burpee" #"4cf4bca5-7cd0-4db8-af11-5d39d485dba8" # aka session_ids in reprocessSessions.py
+    session_name = "27332dae-e4a6-40bb-8c58-2a34d5d3a0e4" #"4cf4bca5-7cd0-4db8-af11-5d39d485dba8" # aka session_ids in reprocessSessions.py
 
     calib_id = "dynamic"#"36598d50-4fd8-406d-8808-4e9df3cd0e84" # None (auto-selected trial), [] (skip), or string of specific trial_id
     static_id = "ac82774d-b679-4a90-bce1-b3b768532503" # None (auto-selected trial), [] (skip), or string of specific trial_id
-    dynamic_trialNames = "600b7a36-5f4a-4a35-8912-58488b6012c6"#"afca93fd-9753-4bea-9130-5fdcf151d9f0" # None (all dynamic trials), [] (skip), or list of trial names. 
+    dynamic_trialNames = "26964f1a-6eb0-44f1-ae28-fbe80d0e3fbc"#"afca93fd-9753-4bea-9130-5fdcf151d9f0" # None (all dynamic trials), [] (skip), or list of trial names. 
     # OBS!!! Above are CaSE Sensitive. dynamic_trialNames are the names of the video files (a uuid)
-    trial_Names = "Burpee_1" #calibration for extrinsic trials." neutral for static trials. NB: Name of the folder the trial are in actually,....
+    trial_Names = "s2_burpee_jump_forward" #calibration for extrinsic trials." neutral for static trials. NB: Name of the folder the trial are in actually,....
     # Trial name for dynamic trials
 
-    poseDetector = 'OpenPose' #OpenPose or hrnet
+    poseDetector = 'hrnet' #OpenPose or hrnet
 
     resolutionPoseDetection = '1x736_2scales'#'1x736_2scales'
 
@@ -106,6 +106,7 @@ if __name__=="__main__":
     #os.chdir('..')
     
     print(extrinsicTrial)
+    cameras_to_use=['Cam1', 'Cam2', 'Cam3']
     main(session_name, trial_Names, dynamic_trialNames, cameras_to_use=['all'],
              intrinsicsFinalFolder='Deployed', isDocker=False,
              extrinsicsTrial=extrinsicTrial,
